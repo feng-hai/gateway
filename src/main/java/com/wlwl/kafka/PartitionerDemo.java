16e8dbec5322275b1af565410cb57d7f437df1c4
fenghai <feng_hai@aliyun.com>
2017-10-25 17:16:43 +0800

    修改过滤器

--- src/main/java/com/wlwl/kafka/PartitionerDemo.java
+++ src/main/java/com/wlwl/kafka/PartitionerDemo.java
@@ -8,29 +8,10 @@ import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.PartitionInfo;
 
-import kafka.utils.VerifiableProperties;
-
-
 public class PartitionerDemo implements Partitioner {
-	public PartitionerDemo(VerifiableProperties props) {
-
-	}
-
-//	public int partition(Object obj, int numPartitions) {
-//		int partition = 0;
-//		if (obj instanceof String) {
-//			String key=(String)obj;
-//			int offset = key.lastIndexOf('.');
-//			if (offset > 0) {
-//				partition = Integer.parseInt(key.substring(offset + 1)) % numPartitions;
-//			}
-//		}else{
-//			partition = obj.toString().length() % numPartitions;
-//		}
-//		
-//		return partition;
-//	}
 	
+
+
 	  @Override
 	    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
 	        // TODO Auto-generated method stub
